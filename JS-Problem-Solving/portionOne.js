90b65aa2058c5901f611835a91950f5abec2b30c
Mohosin999 <programmermohosin@gmail.com>
2023-06-04 17:25:07 +0600

    problem solving codes

--- JS-Problem-Solving/portionOne.js
+++ JS-Problem-Solving/portionOne.js
@@ -1,13 +1,44 @@
-// problem and solve :
-function reverseNumber(n, b) {
-  // Convert n to a string in base b
-  let numString = n.toString(b);
+// ps: 01
+function makeLatinSquare(n) {
+  // Create an empty n x n matrix
+  const square = Array.from({ length: n }, () => []);
 
-  // Reverse the string
-  let reversedString = numString.split("").reverse().join("");
+  // Fill the matrix with the numbers 1 to n
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      square[i][j] = ((i + j) % n) + 1;
+    }
+  }
 
-  // Convert the reversed string back to an integer in base 10
-  let reversedNumber = parseInt(reversedString, b);
-
-  return reversedNumber;
+  return square;
 }
+
+// another solution 01
+const makeLatinSquare1 = (n) => {
+  let square = new Array(n).fill(0).map(() => new Array(n).fill(0));
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      square[i][j] = ((i + j) % n) + 1;
+    }
+  }
+
+  return square;
+};
+
+// console.log(generateLatinSquare(4));
+// Output:
+// [[1, 2, 3, 4],
+//  [2, 3, 4, 1],
+//  [3, 4, 1, 2],
+//  [4, 1, 2, 3]]
+
+// console.log(generateLatinSquare(7));
+// Output:
+// [[1, 2, 3, 4, 5, 6, 7],
+//  [2, 3, 4, 5, 6, 7, 1],
+//  [3, 4, 5, 6, 7, 1, 2],
+//  [4, 5, 6, 7, 1, 2, 3],
+//  [5, 6, 7, 1, 2, 3, 4],
+//  [6, 7, 1, 2, 3, 4, 5],
+//  [7, 1, 2, 3, 4, 5, 6]]
