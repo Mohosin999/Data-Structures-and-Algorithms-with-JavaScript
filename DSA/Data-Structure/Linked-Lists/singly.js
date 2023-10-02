957981cc4ce7b8b53347cde48f449440451cb340
Mohosin999 <mohosin.h.akash@gmail.com>
2023-10-02 11:28:45 +0600

    Apply singly linked list - insert data at the given position

--- DSA/Data-Structure/Linked-Lists/singly.js
+++ DSA/Data-Structure/Linked-Lists/singly.js
@@ -1,5 +1,6 @@
 // Linked List Class
 let head; // head of list
+console.log(head);
 
 /* Node Class */
 class Node {
@@ -7,6 +8,7 @@ class Node {
   constructor(d) {
     this.data = d;
     this.next = null;
+    console.log(this);
   }
 }
 
@@ -23,11 +25,33 @@ function push(new_data) {
 
   /* 4. Move the head to point to new Node */
   head = new_node;
-  //   console.log(head); // Node { data: 10, next: undefined}
+  // console.log(head); // Node { data: 10, next: undefined}
 }
 // push(10);
 
 /**
  * Way - 02
- * Insert a Node at the Front/Beginning of Linked List
+ * Insert a Node after a Given Node in Linked List
+ */
+function insertAfter(prevNode, newData) {
+  /* 1. Check if the given Node is null */
+  if (prev_node == null) {
+    document.write("The given previous node cannot be null");
+    return;
+  }
+
+  /* 2. Allocate the Node &
+    3. Put in the data*/
+  const newNode = new Node(newData);
+
+  /* 4. Make next of new Node as next of prev_node */
+  newNode.next = prevNode.next;
+
+  /* 5. make next of prev_node as new_node */
+  prevNode.next = newNode;
+}
+
+/**
+ * Way - 03
+ * Insert a Node at the End of Linked List
  */
