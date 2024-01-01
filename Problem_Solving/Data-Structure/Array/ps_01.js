8870a7858d8b70f3f01cdf990ff10ddf03b6f8e2
Mohosin999 <mohosin.h.akash@gmail.com>
2024-01-01 10:01:17 +0600

    solved the problem - Find minimum and maximum element in an array

--- Problem_Solving/Data-Structure/Array/ps_01.js
+++ Problem_Solving/Data-Structure/Array/ps_01.js
@@ -17,7 +17,48 @@ function reverseArray(arr) {
   return arr;
 }
 
-// Example usage:
 const originalArray = [1, 2, 3, 4, 5];
 reverseArray(originalArray);
 console.log(originalArray);
+
+// Find minimum and maximum element in an array
+// process 01 ( process 01 is more effective )
+function minmaxNum1(arr, n) {
+  if (n === 0) {
+    return { min: undefined, max: undefined };
+  }
+
+  let min = arr[0];
+  let max = arr[0];
+
+  for (let i = 1; i < n; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+
+  return { min, max };
+}
+
+const arr1 = [1, 4, 2, 3, 7, 5];
+const result = minmaxNum1(arr1, arr1.length - 1);
+console.log(result);
+
+// process 02
+function minmaxNum2(arr) {
+  const minmax = {};
+
+  arr.sort((a, b) => a - b);
+  minmax.min = arr[0];
+  minmax.max = arr[arr.length - 1];
+
+  return minmax;
+}
+
+const arr2 = [1, 4, 2, 3, 7, 5];
+const result2 = minmaxNum2(arr2);
+console.log(result2);
