338e0f7bfa765881d46a45c674791690bad393a9
Mohosin999 <mohosin.h.akash@gmail.com>
2024-01-05 16:34:10 +0600

    changed the file name

--- code.js
+++
@@ -1,4 +0,0 @@
-const x = [1, 4, 6, 0, -9, -5];
-
-console.log(x.sort());
-console.log(x.sort((a, b) => b - a));
---
+++ w.js
@@ -0,0 +1,29 @@
+// const nums = [3,2,4,9]
+
+// function twoSum(nums, target) {
+//   for (let i = 0; i < nums.length; i++) {
+//     for (let j = i + 1; j < nums.length; j++) {
+//       if (nums[i] + nums[j] === target) {
+//         return [i, j];
+//       }
+//     }
+//   }
+//   // If no solution is found
+//   return [];
+// }
+
+// console.log(twoSum(nums, 11));
+
+const nums = [3, 2, 4, 9];
+
+function twoSum(nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      console.log(nums[i] + nums[j]);
+    }
+  }
+  // If no solution is found
+  return [];
+}
+
+console.log(twoSum(nums, 11));
