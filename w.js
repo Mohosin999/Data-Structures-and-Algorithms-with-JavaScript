735d93cf5fb4ba28a137b340d8d5aeef8d38a258
Mohosin999 <mohosin.h.akash@gmail.com>
2024-01-05 17:18:33 +0600

    initial commit

--- w.js
+++ w.js
@@ -1,34 +1,15 @@
-// const nums = [3,2,4,9]
+const nums = [3, 2, 4, 9];
 
-// function twoSum(nums, target) {
-//   for (let i = 0; i < nums.length; i++) {
-//     for (let j = i + 1; j < nums.length; j++) {
-//       if (nums[i] + nums[j] === target) {
-//         return [i, j];
-//       }
-//     }
-//   }
-//   // If no solution is found
-//   return [];
-// }
-
-// console.log(twoSum(nums, 11));
-
-const nums = [
-  [1, 2, 3],
-  [4, 5, 6],
-  ["akash", "yasin", "ashik"],
-  [null, "", undefined],
-];
-
-function twoSum(nums) {
+function twoSum(nums, target) {
   for (let i = 0; i < nums.length; i++) {
     for (let j = i + 1; j < nums.length; j++) {
-      console.log(nums[j]
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
     }
   }
   // If no solution is found
   return [];
 }
 
-console.log(twoSum(nums));
+console.log(twoSum(nums, 11));
