ccea6609d7fcedfd9c45d2fac00849fed5f430cd
Mohosin999 <mohosin.h.akash@gmail.com>
2024-01-05 18:49:10 +0600

    initial commit

--- w.js
+++ w.js
@@ -1,14 +1,14 @@
-function twoSum(nums, target) {
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
-      if (nums[i] + nums[j] === target) {
-        return [i, j];
-      }
-    }
-  }
-  // If no solution is found
-  return [];
-}
+// function twoSum(nums, target) {
+//   for (let i = 0; i < nums.length; i++) {
+//     for (let j = i + 1; j < nums.length; j++) {
+//       if (nums[i] + nums[j] === target) {
+//         return [i, j];
+//       }
+//     }
+//   }
+//   // If no solution is found
+//   return [];
+// }
 
-const nums = [3, 2, 4, 9];
-console.log(twoSum(nums, 11));
+// const nums = [3, 2, 4, 9];
+// console.log(twoSum(nums, 11));
