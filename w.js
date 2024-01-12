8d15a3581c2890e18279a51aecf76ca94511d426
Mohosin999 <mohosin.h.akash@gmail.com>
2024-01-12 15:47:06 +0600

    trying to solve a problem

--- w.js
+++ w.js
@@ -1,35 +1,58 @@
-// function twoSum(nums, target) {
-//   for (let i = 0; i < nums.length; i++) {
-//     for (let j = i + 1; j < nums.length; j++) {
-//       if (nums[i] + nums[j] === target) {
-//         return [i, j];
-//       }
-//     }
-//   }
-//   // If no solution is found
-//   return [];
-// }
+// /**
+//  * @param {number[]} nums
+//  * @return {number}
+//  */
+// var majorityElement = function(nums) {
+//   let dict = {};
 
-// const nums = [3, 2, 4, 9];
-// console.log(twoSum(nums, 11));
+// // Count the occurrences of each element
+// nums.forEach((element) => {
+// if (!dict.hasOwnProperty(element)) {
+//   dict[element] = 1;
+// } else dict[element]++;
+// });
 
-const namedArr = [
-  ["aaa", "bbb", "ccc"],
-  ["AAA", "BBB", "CCC"],
-  ["111", "222", "333"],
-];
+// //{ '2': 1, '3': 2 } - 1st test case
+// //{ '1': 3, '2': 4 } - 2nd test case
 
-const getElement = (arr) => {
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr[i].length; j++) {
-      if (arr[i][j] === "CCC") {
-        console.log("Index of the element is -> ", i, j);
-        return;
-      }
+// // Find the element with the maximum count
+// let keys=Object.keys(dict)
+// keys.sort((a,b)=> dict[b]-dict[a])
+// return keys[0]
+// };
+function majorityElement(nums) {
+  // Initialize the candidate and its count
+  let candidate = null;
+  let count = 0;
+
+  // Iterate through the array
+  for (const num of nums) {
+    // If count is zero, set the current element as the candidate
+    if (count === 0) {
+      candidate = num;
+    }
+
+    // If the current element is the same as the candidate, increment the count
+    if (num === candidate) {
+      count++;
+    } else {
+      // If the current element is different, decrement the count
+      count--;
     }
   }
-  console.log("Element not found!");
-  console.log("Element not found!");
-  console.log("Element not found!");
-  console.log("Element not found!");
-};
+
+  // At this point, 'candidate' is a potential majority element
+  // Verify if it appears more than n/2 times
+  count = 0;
+  for (const num of nums) {
+    if (num === candidate) {
+      count++;
+    }
+  }
+
+  if (count > nums.length / 2) {
+    return candidate;
+  } else {
+    return null; // No majority element
+  }
+}
